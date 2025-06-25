a3e91e27942202daa42a1de3d5defb6a15ea5e58
Haseeb Raza <haseeb.javed715@gmail.com>
2025-06-26 00:18:42 +0200

    Fix local development setup and prepare for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/evencia/',
+  base: process.env.NODE_ENV === 'production' ? '/evencia/' : '/',
   plugins: [react()]
 })
